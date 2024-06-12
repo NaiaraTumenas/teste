599b31712b20e5cb70e91b3b2892158c8baaeb2f
Naiara <naiarasouza1655@gmail.com>
2024-06-12 20:02:09 -0300

    Uma leve ajudinha
    Delta

--- aula 03/script.js
+++ aula 03/script.js
@@ -7,5 +7,12 @@ meuElemento.classList.remove ("square");
 
 function tornarQuadrado() {
 meuElemento.classList.add ("square");
-meuElemento.classList.remove ("circle");
+meuElemento.classList.remove ("circle"); 
+}
+
+function alternarFormato(){
+meuElemento.classList.toggle("circle"); // toggle = olha o estado real que está a caixa e faz o processo inverso//
+meuElemento.classList.toggle("square"); //se estiver quadrado, ele torna círculo. Caso esteja com o círculo, ele torna quadrado//
+
+
 }
